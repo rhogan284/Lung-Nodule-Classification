3fd8fdfb7336039481398ec6e4815cc8511e6900
ryanhogan <ryanhogan284@gmail.com>
2023-09-07 12:39:36 +1000

    Updates

---
+++ Full Database Model/RunningTests.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+images = np.load('nodule_images.npy')
+labels = np.load('malignancy_scores.npy')
+
+unique_values, counts = np.unique(labels, return_counts=True)
+
+for value, count in zip(unique_values, counts):
+    print(f"{value} occurs {count} times")
+
